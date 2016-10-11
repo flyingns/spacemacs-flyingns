1233b29e1ca9b1aa216456fa04dcb63b830dbb57
Bruce Chen <flyingns@gmail.com>
2016-10-11 12:53:22 +0800

    Init for me

--- custom.el
+++ custom.el
@@ -6,13 +6,22 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ahs-case-fold-search nil t)
+ '(ahs-default-range (quote ahs-range-display) t)
+ '(ahs-idle-interval 0.25 t)
+ '(ahs-idle-timer 0 t)
+ '(ahs-inhibit-face-list nil t)
+ '(beacon-blink-delay 0.3)
+ '(blink-cursor-mode nil)
+ '(cfs--current-profile-name "profile1" t)
+ '(column-number-mode t)
  '(command-log-mode-window-size 50)
  '(company-dabbrev-minimum-length 3)
  '(company-dabbrev-other-buffers nil)
+ '(company-quickhelp-mode t)
  '(company-show-numbers t)
  '(company-statistics-auto-restore nil)
  '(ctags-update-delay-seconds 1024)
- '(erc-nick "zilongshanren")
+ '(erc-nick "mls")
  '(erc-port 6666)
  '(evil-want-C-i-jump t)
  '(evil-want-Y-yank-to-eol t)
@@ -20,6 +29,7 @@
  '(expand-region-contract-fast-key "V")
  '(expand-region-exclude-text-mode-expansions (quote (html-mode nxml-mode web-mode)))
  '(expand-region-reset-fast-key "r")
+ '(flycheck-display-errors-delay 0.4)
  '(global-command-log-mode nil)
  '(golden-ratio-exclude-modes
    (quote
@@ -27,10 +37,11 @@
  '(helm-buffer-max-length 56)
  '(helm-move-to-line-cycle-in-source t)
  '(ivy-height 18)
+ '(js2-include-jslint-globals nil)
  '(lua-documentation-url "http://www.lua.org/manual/5.3/manual.html")
  '(magit-use-overlays nil)
  '(only-global-abbrevs t)
- '(org-agenda-custom-commands nil)
+ '(org-agenda-custom-commands nil t)
  '(org-agenda-ndays 1)
  '(org-agenda-show-all-dates t)
  '(org-agenda-skip-deadline-if-done t)
@@ -45,15 +56,24 @@
  '(org-reverse-note-order t)
  '(package-selected-packages
    (quote
-    (glsl-mode pug-mode magithub editorconfig dockerfile-mode docker tablist docker-tramp helm-projectile xterm-color shell-pop eshell-z eshell-prompt-extras esh-help graphviz-dot-mode py-isort dumb-jump restclient racket-mode faceup projectile-rails ob-http helm-gtags feature-mode company-auctex rvm ruby-tools ruby-test-mode rubocop rspec-mode robe rbenv rake enh-ruby-mode chruby bundler inf-ruby yapfify sicp helm-mode-manager org origami tiny evil-unimpaired helm-pydoc unicode-whitespace org-projectile github-search flycheck-clojure evil-escape mwim helm-github-stars fcitx solarized-theme tide typescript-mode spaceline powerline org-plus-contrib ivy-hydra helm-gitignore helm-flx helm-descbinds helm-css-scss helm-company helm-ag helm helm-core flyspell-correct-ivy color-identifiers-mode ag bracketed-paste paradox inflections cider names yaml-mode which-key wgrep uuidgen toc-org smex smeargle smartparens reveal-in-osx-finder restart-emacs ranger pytest py-yapf prodigy persp-mode pcre2el osx-trash org-pomodoro mmm-mode markdown-mode lua-mode live-py-mode link-hint launchctl js2-mode jade-mode info+ ibuffer-projectile projectile hy-mode htmlize hl-todo help-fns+ haml-mode gnuplot gitignore-mode github-clone popup git-gutter-fringe+ git-gutter+ flyspell-correct flycheck evil-visual-mark-mode evil-magit magit-popup git-commit with-editor evil-indent-plus iedit evil-ediff evil undo-tree diminish diff-hl counsel swiper ivy tern company column-enforce-mode cmake-mode clojure-snippets eval-sexp-fu pkg-info clojure-mode bind-map bind-key yasnippet auto-compile packed anaconda-mode pythonic ace-window ace-link avy quelpa package-build wrap-region visual-regexp-steroids visual-regexp peep-dired osx-dictionary nodejs-repl litable keyfreq gulpjs find-file-in-project etags-select ctags-update beacon 4clojure moe-theme edn paredit queue peg json-rpc dash-functional web-completion-data makey anzu highlight goto-chg flx gh logito pcache pos-tip guide-key request parent-mode simple-httpd json-snatcher json-reformat multiple-cursors moz ctable orglue epic alert log4e gntp spinner epl hydra async deferred f s chinese-word-at-point dash youdao-dictionary ws-butler window-numbering web-mode web-beautify volatile-highlights vi-tilde-fringe use-package tagedit smooth-scrolling slim-mode scss-mode sass-mode rfringe reveal-in-finder rainbow-mode rainbow-identifiers rainbow-delimiters pyvenv pyenv-mode popwin pip-requirements persp-projectile pbcopy page-break-lines ox-reveal org-repo-todo org-present org-octopress org-mac-link org-download org-bullets open-junk-file neotree multi-term moz-controller move-text monokai-theme markdown-toc magit macrostep lispy linum-relative leuven-theme less-css-mode json-mode js2-refactor js-doc indent-guide impatient-mode ido-vertical-mode hungry-delete hl-anything highlight-parentheses highlight-numbers highlight-indentation guide-key-tip google-translate golden-ratio github-browse-file gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link gist gh-md ggtags geiser fringe-helper flycheck-ycmd flycheck-pos-tip flx-ido fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-tutor evil-terminal-cursor-changer evil-surround evil-search-highlight-persist evil-org evil-numbers evil-nerd-commenter evil-matchit evil-lisp-state evil-jumper evil-indent-textobject evil-iedit-state evil-exchange evil-args evil-anzu engine-mode emmet-mode elisp-slime-nav elfeed discover-my-major deft dash-at-point cython-mode company-ycmd company-web company-tern company-statistics company-quickhelp company-c-headers company-anaconda command-log-mode coffee-mode cmake-font-lock clj-refactor clean-aindent-mode clang-format cider-eval-sexp-fu chinese-fonts-setup buffer-move auto-yasnippet auto-highlight-symbol auto-dictionary align-cljlet aggressive-indent adaptive-wrap ace-jump-mode ac-ispell 2048-game)))
+    (pug-mode yapfify rake py-isort org-projectile mwim livid-mode skewer-mode lispy intero hlint-refactor projectile helm-hoogle github-search marshal ht flyspell-correct-helm evil-unimpaired with-editor smartparens undo-tree eshell-z dumb-jump company-ghci company-emacs-eclim command-log-mode color-identifiers-mode cmake-mode s auto-complete package-build company-auctex auctex-latexmk auctex simple-httpd yaml-mode xterm-color ws-butler web-mode stickyfunc-enhance srefactor spaceline powerline shm shell-pop rvm ruby-tools ruby-test-mode rubocop rspec-mode robe rcirc-notify rcirc-color rbenv ranger persp-mode pangu-spacing pandoc-mode ox-pandoc orgit org-tree-slide org-plus-contrib magit-gitflow magit-gh-pulls lorem-ipsum zoutline swiper ledger-mode hindent helm-themes helm-swoop helm-pydoc helm-projectile helm-mode-manager helm-make helm-gtags helm-gitignore helm-flyspell helm-flx helm-descbinds helm-css-scss helm-company helm-c-yasnippet helm-ag haskell-snippets git-gutter-fringe git-gutter flycheck-ledger flycheck-haskell find-by-pinyin-dired evil-mc evil-escape eshell-prompt-extras esh-help enh-ruby-mode emoji-cheat-sheet-plus ein request websocket define-word company-ghc ghc haskell-mode company-emoji company-cabal cmm-mode chruby chinese-pyim chinese-pyim-basedict bundler inf-ruby ace-pinyin pinyinlib ace-jump-helm-line helm helm-core which-key org eclim ag bracketed-paste paradox inflections cider names wgrep uuidgen toc-org smex smeargle reveal-in-osx-finder restart-emacs pytest py-yapf prodigy pcre2el osx-trash org-pomodoro mmm-mode markdown-mode lua-mode live-py-mode link-hint launchctl js2-mode jade-mode info+ ibuffer-projectile hy-mode htmlize hl-todo help-fns+ haml-mode gnuplot gitignore-mode github-clone popup git-gutter-fringe+ git-gutter+ flyspell-correct flycheck fcitx evil-visual-mark-mode evil-magit magit-popup git-commit evil-indent-plus iedit evil-ediff evil diminish diff-hl counsel ivy tern company column-enforce-mode clojure-snippets eval-sexp-fu pkg-info clojure-mode bind-map bind-key yasnippet auto-compile packed anaconda-mode pythonic ace-window ace-link avy quelpa wrap-region visual-regexp-steroids visual-regexp peep-dired osx-dictionary nodejs-repl litable keyfreq gulpjs find-file-in-project etags-select ctags-update beacon 4clojure moe-theme edn paredit queue peg json-rpc dash-functional web-completion-data makey anzu highlight goto-chg flx gh logito pcache pos-tip guide-key parent-mode json-snatcher json-reformat multiple-cursors moz ctable orglue epic alert log4e gntp spinner epl hydra async deferred f chinese-word-at-point dash youdao-dictionary window-numbering web-beautify volatile-highlights vi-tilde-fringe use-package tagedit smooth-scrolling slim-mode scss-mode sass-mode rfringe reveal-in-finder rainbow-mode rainbow-identifiers rainbow-delimiters pyvenv pyenv-mode popwin pip-requirements persp-projectile pbcopy page-break-lines ox-reveal org-repo-todo org-present org-octopress org-mac-link org-download org-bullets open-junk-file neotree multi-term moz-controller move-text monokai-theme markdown-toc magit macrostep linum-relative leuven-theme less-css-mode json-mode js2-refactor js-doc indent-guide impatient-mode ido-vertical-mode hungry-delete hl-anything highlight-parentheses highlight-numbers highlight-indentation guide-key-tip google-translate golden-ratio github-browse-file gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link gist gh-md ggtags geiser fringe-helper flycheck-ycmd flycheck-pos-tip flx-ido fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-tutor evil-terminal-cursor-changer evil-surround evil-search-highlight-persist evil-org evil-numbers evil-nerd-commenter evil-matchit evil-lisp-state evil-jumper evil-indent-textobject evil-iedit-state evil-exchange evil-args evil-anzu engine-mode emmet-mode elisp-slime-nav elfeed discover-my-major deft dash-at-point cython-mode company-ycmd company-web company-tern company-statistics company-quickhelp company-c-headers company-anaconda coffee-mode cmake-font-lock clj-refactor clean-aindent-mode clang-format cider-eval-sexp-fu chinese-fonts-setup buffer-move auto-yasnippet auto-highlight-symbol auto-dictionary align-cljlet aggressive-indent adaptive-wrap ace-jump-mode ac-ispell 2048-game)))
  '(paradox-github-token t)
+ '(pyim-dicts
+   (quote
+    ((:name "BigDict-01" :file "/home/mls/.spacemacs.d/pyim/dicts/vimim_sogou.txt" :coding utf-8-unix :dict-type pinyin-dict)
+     (:name "BigDict-01" :file "/Users/guanghui/.emacs.d/.cache/pyim-bigdict.pyim" :coding utf-8-unix))))
+ '(pyim-use-tooltip nil)
  '(ring-bell-function (quote ignore))
  '(safe-local-variable-values
    (quote
     ((eval setenv "PYTHONPATH" "/Users/guanghui/cocos2d-x/tools/cocos2d-console/plugins:/Users/guanghui/cocos2d-x/tools/cocos2d-console/bin"))))
  '(sp-show-pair-from-inside t t)
+ '(tags-add-tables nil)
  '(tags-revert-without-query t)
+ '(tool-bar-mode nil)
  '(vc-follow-symlinks t)
+ '(wakatime-api-key "239a7e4f-c32c-4d67-a43e-279d7fa14ef9")
+ '(wakatime-cli-path "/home/mls/")
  '(web-mode-markup-indent-offset 2)
  '(ycmd-extra-conf-handler (quote load))
  '(ycmd-extra-conf-whitelist (quote ("~/cocos2d-x/*"))))
@@ -75,5 +95,83 @@
  '(mc/cursor-bar-face ((t (:background "chartreuse3"))))
  '(show-paren-match ((t (:background "dark gray" :foreground "#d33682" :weight bold))))
  '(sp-show-pair-match-face ((t (:background "#272822" :foreground "gray" :inverse-video t :weight normal)))))
+;(setq org-bullets-bullet-list '("◉" "○" "✸" "✿" "☢" "❀" "✚" "✜" "◖"))
+(setq org-bullets-bullet-list '("①" "②" "③" "④" "⑤" "⑦" "⑧" "⑨"))
+(setq org-src-tab-acts-natively t)
+(setq org-agenda-files (quote ("~/Nutstore/org/anniversary.org"
+							   "~/Nutstore/org/business.org"
+							   "~/Nutstore/org/organizer.org"
+							   "~/Nutstore/org/finance.org"
+							   "~/Nutstore/org/todo.org"
+							   "~/Nutstore/org/someday.org"
+							   "~/Nutstore/org/project.org"
+							   "~/Nutstore/org/tasks.org")))
+;; (setq org-agenda-files (list "~/Nutstore/org"))
+(setq org-mobile-files (list "~/Nutstore/org/organizer.org"
+							 "~/Nutstore/org/todo.org"
+							 "~/Nutstore/org/finance.org"
+							 "~/Nutstore/org/someday.org"
+							 "~/Nutstore/org/project.org"
+							 "~/Nutstore/org/tasks.org"))
+(setq org-default-notes-file "~/Nutstore/org/notes.org")
+
+(setq org-capture-templates
+	  '(("t" "Todo" entry (file+headline "~/Nutstore/org/todo.org" "Workspace")
+		 "* TODO %?\n  %i"
+		 :empty-lines 1)
+        ("L" "Ledger" entry (file+headline "~/Nutstore/org/finance.org" "Finance")
+         "* TODO %?\n"
+         :empty-lines 1)
+        ("n" "notes" entry (file+headline "~/Nutstore/org/notes.org" "Quick notes")
+         "* TODO [#C] %?\n  %i\n %U"
+         :empty-lines 1)
+        ("b" "Blog Ideas" entry (file+headline "~/Nutstore/org/notes.org" "Blog Ideas")
+         "* TODO %?\n  %i\n %U"
+         :empty-lines 1)
+        ("w" "work" entry (file+headline "~/Nutstore/org/tasks.org" "Work")
+         "* TODO %?\n  %i\n %U"
+         :empty-lines 1)
+        ("c" "Chrome" entry (file+headline "~/Nutstore/org/notes.org" "Quick notes")
+         "* TODO %?\n %(zilongshanren/retrieve-chrome-current-tab-url)\n %i\n %U"
+         :empty-lines 1)
+        ("l" "links" entry (file+headline "~/Nutstore/org/notes.org" "Quick notes")
+         "* TODO %?\n  %i\n %a \n %U"
+         :empty-lines 1)
+        ("j" "Journal Entry"
+         entry (file+datetree "~/Nutstore/org/journal.org")
+         "* %?"
+         :empty-lines 1)))
+(setq org-agenda-custom-commands
+      '(
+        ("w" . "任务安排")
+        ("wa" "重要且紧急的任务" tags-todo "+PRIORITY=\"A\"")
+        ("wb" "重要且不紧急的任务" tags-todo "-Weekly-Monthly-Daily+PRIORITY=\"B\"")
+        ("wc" "不重要且紧急的任务" tags-todo "+PRIORITY=\"C\"")
+        ("b" "Blog" tags-todo "BLOG")
+        ("p" . "项目安排")
+        ("pw" tags-todo "PROJECT+WORK+CATEGORY=\"cocos2d-x\"")
+        ("pl" tags-todo "PROJECT+DREAM+CATEGORY=\"zilongshanren\"")
+        ("W" "Weekly Review"
+         ((stuck "")            ;; review stuck projects as designated by org-stuck-projects
+          (tags-todo "PROJECT") ;; review all projects (assuming you use todo keywords to designate projects)
+          ))))
+		;;; 补充用法: holiday-float m w n 浮动阳历节日, m 月的第 n 个星期 w%7
+(setq general-holidays '((holiday-fixed 1 1   "元旦")
+                         (holiday-fixed 2 14  "情人节")
+                         (holiday-fixed 4 1   "愚人节")
+                         (holiday-fixed 12 25 "圣诞节")
+                         (holiday-fixed 10 1  "国庆节")
+                         (holiday-float 5 0 2 "母亲节")   ;5月的第二个星期天
+                         (holiday-float 6 0 3 "父亲节")))
+(setq local-holidays '((holiday-chinese 1 15  "元宵节 (正月十五)")
+                       (holiday-chinese 5 5   "端午节 (五月初五)")
+                       (holiday-chinese 9 9   "重阳节 (九月初九)")
+                       (holiday-chinese 8 15  "中秋节 (八月十五)")
+                       ;; 生日
+                       ;; (birthday-fixed 9 28  "爸爸生日(1950)")
+                       ;; (birthday-fixed 10 1  "妈妈生日(1953)")
+                       (holiday-chinese 3 2 "宝宝生日")
+                       (holiday-chinese 4 23 "老婆生日") ;阴历生日
+                       ))
 ;; Do not write anything past this comment. This is where Emacs will
 ;; auto-generate custom variable definitions.
